76dadb04232c41330216de62068b471229174588
Pranav Gupta <guptapranav4299@gmail.com>
2021-05-16 11:58:42 +0530

    Updates

---
+++ precache-manifest.0d3534209a335addff564d03ffff8434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2ca68b253b09ba9b828aa38b3fd366",
+    "url": "/voice-news-app/index.html"
+  },
+  {
+    "revision": "f1022317229395b32c70",
+    "url": "/voice-news-app/static/css/main.024e2b10.chunk.css"
+  },
+  {
+    "revision": "0c3ab0c80ca8e3e64624",
+    "url": "/voice-news-app/static/js/2.603a76d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voice-news-app/static/js/2.603a76d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1022317229395b32c70",
+    "url": "/voice-news-app/static/js/main.2d92c2e0.chunk.js"
+  },
+  {
+    "revision": "634ef501682fa6086568",
+    "url": "/voice-news-app/static/js/runtime-main.7606ec15.js"
+  }
+]);
